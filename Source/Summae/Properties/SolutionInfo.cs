ad3ccb3974e11465101ead1129d64fca29edc24c
Josip Medved <jmedved@jmedved.com>
2011-09-22 21:45:58 +0200

    Fixing copyright notice.

--- Source/Summae/Properties/SolutionInfo.cs
+++ Source/Summae/Properties/SolutionInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("Summae")]
 [assembly: AssemblyCompany("Josip Medved")]
-[assembly: AssemblyCopyright("Copyright (c) 2009-2010 Josip Medved <jmedved@jmedved.com>")]
+[assembly: AssemblyCopyright("Copyright (c) 2009 Josip Medved <jmedved@jmedved.com>")]
 
 [assembly: AssemblyVersion("1.50.11606.20353")]
 [assembly: AssemblyInformationalVersion("1.50")]
