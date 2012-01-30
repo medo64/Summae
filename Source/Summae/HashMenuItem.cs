3712e97667f6e11969548b8251331f2190b07768
Josip Medved <jmedved@jmedved.com>
2012-01-29 23:38:34 -0500

    Adding forgotten file.

---
+++ Source/Summae/HashMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Summae {
+    internal class HashMenuItem : ToolStripMenuItem {
+
+        public HashMenuItem(string key, string text, bool isChecked) {
+            this.Key = key;
+            this.Text = text;
+            this.Checked = isChecked;
+        }
+
+
+        protected override void OnClick(EventArgs e) {
+            this.Checked = !this.Checked;
+        }
+
+        public string Key { get; private set; }
+
+    }
+}
