d5274672944471731a28a24de2f28dd6cc46e6ad
Josip Medved <jmedved@jmedved.com>
2018-12-02 14:06:57 -0800

    Refactoring context menu setup.

--- Source/Summae/SettingsForm.cs
+++ Source/Summae/SettingsForm.cs
@@ -1,10 +1,10 @@
-using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Reflection;
 using System.Security.AccessControl;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace Summae {
     internal partial class SettingsForm : Form {
@@ -12,25 +12,10 @@ namespace Summae {
             InitializeComponent();
             this.Font = SystemFonts.MessageBoxFont;
         }
-        
+
 
         private void SettingsForm_Load(object sender, EventArgs e) {
-            var subCommandList = new List<string>();
-
-            using (var rk = Registry.ClassesRoot.OpenSubKey(@"*\shell", writable: false)) {
-                if (rk != null) {
-                    using (var key = rk.OpenSubKey("Summae", writable: false)) {
-                        if (key != null) {
-                            var subCommands = key.GetValue("SubCommands");
-                            if (subCommands != null) {
-                                foreach (var subCommand in subCommands.ToString().Split(';')) {
-                                    subCommandList.Add(subCommand);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            var entries = new List<ShellEntry>();
 
             using (var rk = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*\shell", writable: false)) {
                 if (rk != null) {
@@ -39,7 +24,7 @@ namespace Summae {
                             var subCommands = key.GetValue("SubCommands");
                             if (subCommands != null) {
                                 foreach (var subCommand in subCommands.ToString().Split(';')) {
-                                    subCommandList.Add(subCommand);
+                                    entries.Add(ShellEntry.GetEntry(subCommand));
                                 }
                             }
                         }
@@ -47,28 +32,32 @@ namespace Summae {
                 }
             }
 
-            chbCrc16.Checked = subCommandList.Contains("Summae.Crc16");
-            chbCrc32.Checked = subCommandList.Contains("Summae.Crc32");
-            chbMd5.Checked = subCommandList.Contains("Summae.Md5");
-            chbRipeMd160.Checked = subCommandList.Contains("Summae.RipeMd160");
-            chbSha1.Checked = subCommandList.Contains("Summae.Sha1");
-            chbSha256.Checked = subCommandList.Contains("Summae.Sha256");
-            chbSha384.Checked = subCommandList.Contains("Summae.Sha384");
-            chbSha512.Checked = subCommandList.Contains("Summae.Sha512");
+            chbCrc16.Checked = entries.Contains(ShellEntry.Crc16);
+            chbCrc32.Checked = entries.Contains(ShellEntry.Crc32);
+            chbMd5.Checked = entries.Contains(ShellEntry.Md5);
+            chbRipeMd160.Checked = entries.Contains(ShellEntry.RipeMd160);
+            chbSha1.Checked = entries.Contains(ShellEntry.Sha1);
+            chbSha256.Checked = entries.Contains(ShellEntry.Sha256);
+            chbSha384.Checked = entries.Contains(ShellEntry.Sha384);
+            chbSha512.Checked = entries.Contains(ShellEntry.Sha512);
         }
 
         private void btnOK_Click(object sender, EventArgs e) {
-            var subCommandList = new List<string>();
-            if (chbCrc16.Checked) { subCommandList.Add("Summae.Crc16"); }
-            if (chbCrc32.Checked) { subCommandList.Add("Summae.Crc32"); }
-            if (chbMd5.Checked) { subCommandList.Add("Summae.Md5"); }
-            if (chbRipeMd160.Checked) { subCommandList.Add("Summae.RipeMd160"); }
-            if (chbSha1.Checked) { subCommandList.Add("Summae.Sha1"); }
-            if (chbSha256.Checked) { subCommandList.Add("Summae.Sha256"); }
-            if (chbSha384.Checked) { subCommandList.Add("Summae.Sha384"); }
-            if (chbSha512.Checked) { subCommandList.Add("Summae.Sha512"); }
+            var entries = new List<ShellEntry>();
+            if (chbCrc16.Checked) { entries.Add(ShellEntry.Crc16); }
+            if (chbCrc32.Checked) { entries.Add(ShellEntry.Crc32); }
+            if (chbMd5.Checked) { entries.Add(ShellEntry.Md5); }
+            if (chbRipeMd160.Checked) { entries.Add(ShellEntry.RipeMd160); }
+            if (chbSha1.Checked) { entries.Add(ShellEntry.Sha1); }
+            if (chbSha256.Checked) { entries.Add(ShellEntry.Sha256); }
+            if (chbSha384.Checked) { entries.Add(ShellEntry.Sha384); }
+            if (chbSha512.Checked) { entries.Add(ShellEntry.Sha512); }
 
+            CreateMenu(entries);
+        }
 
+        private static void CreateMenu(IEnumerable<ShellEntry> entries) {
+            //Create: HKCU\Software\Classes\*
             using (var rk = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*", RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl)) { //Windows 7 might not have star (*) registry entry
                 if (rk == null) {
                     using (var rkStar = Registry.CurrentUser.OpenSubKey(@"Software\Classes", RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl)) {
@@ -77,6 +66,7 @@ namespace Summae {
                 }
             }
 
+            //Delete: HKCU\Software\Classes\*\shell\Summae (and Create: HKCU\Software\Classes\*\shell)
             using (var rk = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*\shell", RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl)) {
                 if (rk == null) {
                     using (var rkShell = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*", RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl)) {
@@ -89,19 +79,23 @@ namespace Summae {
                 }
             }
 
+            //Create: Software\Classes\*\shell\Summae (SubCommands)
             using (var rk = Registry.CurrentUser.OpenSubKey(@"Software\Classes\*\shell", RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.FullControl)) {
                 using (var key = rk.CreateSubKey("Summae")) {
                     key.SetValue("Icon", @"""" + Assembly.GetExecutingAssembly().Location + @"""", RegistryValueKind.String);
                     key.SetValue("MUIVerb", @"Summae", RegistryValueKind.String);
-                    if (subCommandList.Count > 0) {
-                        key.SetValue("SubCommands", string.Join(";", subCommandList.ToArray()), RegistryValueKind.String);
+
+                    var subcommandList = new List<string>();
+                    foreach (var entry in entries) {
+                        subcommandList.Add(entry.Command);
+                    }
+                    if (subcommandList.Count > 0) {
+                        key.SetValue("SubCommands", string.Join(";", subcommandList.ToArray()), RegistryValueKind.String);
                     } else {
                         key.SetValue("AppliesTo", @"System.FileName:=\", RegistryValueKind.String); //dummy filename; trick to prevent inheritance from HKEY_CLASSES_ROOT
                     }
                 }
             }
-
         }
-
     }
 }
---
+++ Source/Summae/ShellEntry.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace Summae {
+    internal class ShellEntry {
+
+        private ShellEntry(string name, string title) {
+            this.Name = name;
+            this.Title = title;
+        }
+
+        public string Name { get; }
+        public string Title { get; }
+        public string Command { get { return "Summae." + Name; } }
+
+
+        public static ShellEntry GetEntry(string command) {
+            foreach (var entry in AllEntries) {
+                if (string.Equals(entry.Command, command, StringComparison.Ordinal)) { return entry; }
+            }
+            return null;
+        }
+
+        public static IEnumerable<ShellEntry> AllEntries {
+            get {
+                yield return Crc16;
+                yield return Crc32;
+                yield return Md5;
+                yield return RipeMd160;
+                yield return Sha1;
+                yield return Sha256;
+                yield return Sha384;
+                yield return Sha512;
+            }
+        }
+
+        public static ShellEntry Crc16 { get; } = new ShellEntry("Crc16", "CRC-16");
+        public static ShellEntry Crc32 { get; } = new ShellEntry("Crc32", "CRC-32");
+        public static ShellEntry Md5 { get; } = new ShellEntry("Md5", "MD-5");
+        public static ShellEntry RipeMd160 { get; } = new ShellEntry("RipeMd160", "RIPE MD-160");
+        public static ShellEntry Sha1 { get; } = new ShellEntry("Sha1", "SHA-1");
+        public static ShellEntry Sha256 { get; } = new ShellEntry("Sha256", "SHA-256");
+        public static ShellEntry Sha384 { get; } = new ShellEntry("Sha384", "SHA-384");
+        public static ShellEntry Sha512 { get; } = new ShellEntry("Sha512", "SHA-512");
+
+    }
+}
